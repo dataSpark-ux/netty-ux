71dcf395f5d2cf3c40675aec2fde030119cf7593
wy <837466309@qq.com>
2020-02-01 15:42:35 +0800

    first commit

---
+++ server-app/src/main/java/com/wy/app/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.wy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangyi
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
